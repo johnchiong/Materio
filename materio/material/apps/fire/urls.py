b22ba848ad7d6e89d70647f78f68839ca49a1fa0
johnchiong <152459351+johnchiong@users.noreply.github.com>
2025-05-25 23:57:28 +0800

    materio

---
+++ materio/material/apps/fire/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import DashboardsView, MultilineIncidentTop3Country, multipleBarbySeverity
+from .views import PieCountbySeverity, LineCountFirestations
+
+
+
+urlpatterns = [
+    path("", DashboardsView.as_view(template_name="dashboard_analytics.html"), name="index",), 
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
+    path('lineChart/', LineCountFirestations, name='chart'),
+    path('pieChart/', PieCountbySeverity, name='chart'),
+]
