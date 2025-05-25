54eef7341086f4a32076267f4622a581c47ed76c
johnchiong <152459351+johnchiong@users.noreply.github.com>
2025-05-26 00:35:13 +0800

    gitignore

--- materio/material/apps/fire/urls.py
+++ materio/material/apps/fire/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import DashboardsView, MultilineIncidentTop3Country, multipleBarbySeverity
-from .views import PieCountbySeverity, LineCountFirestations
+from .views import PieCountbySeverity, LineCountFirestations ,FireStationtList
 
 
 
